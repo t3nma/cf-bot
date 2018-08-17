26353c7f103cc619ecb391c730975587504e644b
brunoccast <brvnocasteleiro@gmail.com>
2018-08-17 03:53:46 +0100

    fix index arg handling

--- src/index.js
+++ src/index.js
@@ -25,33 +25,32 @@ client.on('message', async msg => {
 
     /* handle arguments */
     if(cmd.args !== undefined && cmd.args && args.length == 0) {
-        msg.reply('missing args'); // TODO improve notif.
+        msg.reply(`you didn't specify any arguments! Type \`help ${cmd.name}\``
+            + ' for more info about the command');
         return;
     }
 
     /* handle cooldown */
-    if(cmd.cooldown !== undefined) {
-        if(!cooldowns.has(cmd.name)) {
-            cooldowns.set(cmd.name, new Discord.Collection());
-        }
-
-        const now = Date.now();
-        const timestamps = cooldowns.get(cmd.name);
-        const cooldown_amount = (cmd.cooldown || 3) * 1000;
-
-        if(timestamps.has(msg.author.id)) {
-            const expiration_time = timestamps.get(msg.author.id) + cooldown_amount;
-
-            if(now < expiration_time) {
-                const time_left = (expiration_time - now) / 1000;
-                msg.reply(`please wait ${time_left.toFixed(1)} more second(s) before reusing the command`);
-                return;
-            }
-        }
-
-        timestamps.set(msg.author.id, now);
-        setTimeout(() => timestamps.delete(msg.author.id), cooldown_amount);
+    if(!cooldowns.has(cmd.name)) {
+        cooldowns.set(cmd.name, new Discord.Collection());
     }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(cmd.name);
+    const cooldown_amount = (cmd.cooldown || 3) * 1000;
+
+    if(timestamps.has(msg.author.id)) {
+        const expiration_time = timestamps.get(msg.author.id) + cooldown_amount;
+
+        if(now < expiration_time) {
+            const time_left = (expiration_time - now) / 1000;
+            msg.reply(`please wait ${time_left.toFixed(1)} more second(s) before reusing the command`);
+            return;
+        }
+    }
+
+    timestamps.set(msg.author.id, now);
+    setTimeout(() => timestamps.delete(msg.author.id), cooldown_amount);
 
     try {
         cmd.execute(msg, args);
